9d1f0b2ecca2c3b1a6c71740239ca8c667e9401c
AshuRahul1996 <125209625+AshuRahul1996@users.noreply.github.com>
2023-02-27 16:09:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+console.log('welcome to Notes app This is app.js');
+showNotes();
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function(e) {
+    let addTxt = document.getElementById("addTxt");
+    // let addtitle = document.getElementById("addtitle");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+
+    }
+    else {
+        notesObj = JSON.parse(notes);
+
+    }
+    
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    // addtitle.value = "";
+    // console.log(notesObj);
+    showNotes();
+});
+// function to show elements from localStorage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+
+    }
+    else {
+        notesObj = JSON.parse(notes);
+
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+          <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text">${element} </p>
+                        <button id ="${index}" onclick ="deleteNotes(this.id)"class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+
+
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+
+ 
+    else{
+        notesElm.innerHTML=`Nothing to show! use "Add a Note" section above to add notes.`;
+    }
+}
+function deleteNotes(index) {
+    // console.log('i m deleting', index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+
+    }
+    else {
+        notesObj = JSON.parse(notes);
+
+    }
+
+    notesObj.splice(index, 1);
+
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowercase();
+// console.log('input event fired',inputVal);
+
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
+
+        }
+        else {
+            element.style.display = "none";
+        }
+
+    })
+
+})
---
+++ hh
@@ -0,0 +1,15 @@
+ <div id="notes" class="row container-fluid">
+                <div class="noteCards my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note 1</h5>
+                        <p class="card-text">this is good </p>
+                        <button id="0" onclick="deleteNotes(this.id)" class="btn btn-primary">deleteNotes</button>
+                    </div>
+                </div>
+                <div class="noteCards my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note 2</h5>
+                        <p class="card-text">king </p>
+                        <button id="1" onclick="deleteNotes(this.id)" class="btn btn-primary">deleteNotes</button>
+                    </div>
+                </div>
